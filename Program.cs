8c6475b1fa7704a3f2db4554df90c8e072e4a824
Samory Sundjata <samory.sundjata@gmail.com>
2022-03-15 20:42:17 -0300

    Estabelecendo o ack.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AppOrderWorker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
 
 namespace AppOrderWorker
 {
@@ -6,7 +11,44 @@ namespace AppOrderWorker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orderQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        var order = JsonSerializer.Deserialize<Order>(message);
+
+                        Console.WriteLine($"Order: {order.OrderNumber}|{order.ItemName}|{order.Price:N2}");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        //Logger
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                                    
+                };
+
+                channel.BasicConsume(queue: "orderQueue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
